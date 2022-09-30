4c99892ce46f82748c26f351e39ed87451d9354d
kalyan <kalyan@ngpwebsmart.com>
2022-09-30 11:43:20 +0530

    Kalyan Commit

--- script.js
+++ script.js
@@ -6,32 +6,39 @@ const weather_image = document.getElementById('weather-image')
 
 // Fetch data from api 
 const data = async () => {
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${myLocation.value},IN&appid=${api_key}&units=metric`
-		const response = await fetch(url)
-		const data = await response.json()
-		return data
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${myLocation.value},IN&appid=${api_key}&units=metric`
+	const response = await fetch(url)
+	const data = await response.json()
+	return data
 }
 
 // Fetch info from data function
 findButton.onclick = () => {
-	finalResponse = data().then(response=> {
-		if (response.cod=='404') {
+	finalResponse = data().then(response => {
+		if (response.cod == '404') {
 			info_card.innerHTML = `<h4>Either spelling mistake or city not found in India</h4>`
-		}else {
-			console.log(response.weather[0].main)
-			weather_image.innerHTML = `<img id="weather-icon" src="./images/${response.weather[0].main}.png">`
+		} else {
+			console.log(response)
+			const wheather = response.weather[0].main;
+			const array = ['clouds','Rain','sunny'];
+			if(array.includes(wheather)){	
+				weather_image.innerHTML = `<img id="weather-icon" src="./images/${wheather}.png">`
+			}
+			else{
+				weather_image.innerHTML = `<img id="weather-icon" src="./images/general.png">`
+			}
 			info_card.innerHTML = `<h2 id="city-name"><u>${response.name}</u></h2>
 			<p> Weather : <b>${response.weather[0].description}</b></p>
 			<p> Temperature : <b>${response.main.temp}</b> °C</p>
 			<p> Humidity :  <b>${response.main.humidity}%</b></p>
 			<p> Wind Speed : <b>${response.wind.speed}</b> km/h</p>`
 		}
-	})	
+	})
 }
 
 // Listen for Enter key
-document.addEventListener('keydown', function(e){
-	if(e.key=='Enter') {
+document.addEventListener('keydown', function (e) {
+	if (e.key == 'Enter') {
 		findButton.click()
 	}
 })
